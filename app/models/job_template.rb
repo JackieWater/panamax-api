3a12841dd03fa569f88937f424a1a394613d1fc5
Mike Arnold <dharmamike@gmail.com>
2015-01-08 16:31:06 -0600

    adding rake task to load job templates

--- app/models/job_template.rb
+++ app/models/job_template.rb
@@ -2,4 +2,10 @@ class JobTemplate < ActiveRecord::Base
   has_many :job_steps, -> { order(:order) }
 
   scope :cluster_job_templates, -> { where(type: 'ClusterJobTemplate') }
+
+  def self.load_templates(pathname)
+    pathname.each_child do |template|
+      JobTemplateBuilder.create(template.read)
+    end
+  end
 end
